f93959061072c22ff42e6da86e9508c5bc1ec27e
stash4 <high.mackey@gmail.com>
2015-11-01 02:05:49 +0900

    Update getNasneStatus function to get Recording Status

--- tweet_nasne_api.py
+++ tweet_nasne_api.py
@@ -6,45 +6,86 @@ import time
 import twitter
 import twitterKeys
 
-api = twitter.Api(
-	twitterKeys.keys['consumerKey'],
-	twitterKeys.keys['consumerSecret'],
-	twitterKeys.keys['accessToken'],
-	twitterKeys.keys['accessTokenSecret']
-	)
+api = twitter.Api(twitterKeys.keys['consumerKey'],
+	  			  twitterKeys.keys['consumerSecret'],
+				  twitterKeys.keys['accessToken'],
+				  twitterKeys.keys['accessTokenSecret']
+				 )
 
-nasne_ip = "192.168.10.30:64210"
+nasne_ip = "192.168.10.30"
 
-# nasneのAPIを叩く
 def getNasneStatus(nasne_ip):
 	try:
-		hddInfo = urllib2.urlopen("http://" + nasne_ip + "/status/HDDInfoGet?id=0")
+		apiAddr = "http://" + nasne_ip + ":64210/status/"
+		
+		hddInfo = urllib2.urlopen(apiAddr + "HDDInfoGet?id=0")
+		chInfo = urllib2.urlopen(apiAddr + "boxStatusListGet")
+		
 		j_hddInfo = json.loads(hddInfo.read())
+		j_chInfo = json.loads(chInfo.read())
+		
 		hddUsage_Per = round((float(j_hddInfo['HDD']['usedVolumeSize']) / float(j_hddInfo['HDD']['totalVolumeSize'])) * 100, 2)
 		hddFree_GB = round((float(j_hddInfo['HDD']['freeVolumeSize']) / 1024 / 1024 / 1024), 2)
-		nasneStatuses = {"hddUsage": hddUsage_Per,
-						"hddFree": hddFree_GB
-						}
-		return nasneStatuses
+		
+		if j_chInfo['tvTimerInfoStatus']['nowId'] == "":
+			recChannel = ""
+			recTitle = ""
+			recDescription = ""
+		else:
+			recInfo = urllib2.urlopen(apiAddr +
+									  "channelInfoGet2?networkId="+ j_chInfo['tuningStatus']['networkId'] + 
+									  "&transportStreamId=" + j_chInfo['tuningStatus']['transportStreamId'] +
+									  "&serviceId=" + j_chInfo['tuningStatus']['serviceId'] +
+									  "&withDescriptionLong=0"
+									 )
+			j_recInfo = json.loads(recInfo.read())
+			recChannel = j_recInfo['channel']['tsName']
+			recTitle = j_recInfo['channel']['title']
+			recDescription = j_recInfo['channel']['description']
+			
+		nasneStatus = {"hddUsage"	: hddUsage_Per,
+					   "hddFree"	: hddFree_GB,
+					   "channel"	: recChannel,
+					   "title"		: recTitle,
+					   "description": recDescription
+					  }
+		return nasneStatus
+	
 	except:
 		time.sleep(10)
 
-# UserTimeline取得ループ
-since = 1		
-while(True):
-	try:
-		myTimeline = api.GetUserTimeline(count=10,since_id=since)
-		tweetNum = 0
-		for tweet in myTimeline:
-			if(tweetNum == 0):
-				since = tweet.id
-			print(tweet.text)
-			if('nasne' in tweet.text):
-				if('API' in tweet.text):
+
+if __name__ == '__main__':
+	# UserTimeline取得ループ
+	since = 1		
+	while(True):
+		try:
+			myTimeline = api.GetUserTimeline(count=10,since_id=since)
+			
+			tweetNum = 0
+			for tweet in myTimeline:
+				if tweetNum == 0 :
+					since = tweet.id
+				print tweet.text
+				
+				if 'nasne' in tweet.text:
 					ns = getNasneStatus(nasne_ip)
-					content = "@stash_4 nasne status\n" + "HDD usage: " + str(ns['hddUsage']) + "%\n" + "Free space: " + str(ns['hddFree']) + "GB"
-					api.PostUpdate(content.decode('utf-8'), in_reply_to_status_id=tweet.id)
-			tweetNum += 1
-		time.sleep(10)
-	except:
-		time.sleep(30)
+					
+					tweetHdd = "@stash_4\nHDD_Status\nHDD usage: " + str(ns['hddUsage']) + "%\nFree space: " + str(ns['hddFree']) + "GB"
+					api.PostUpdate(tweetHdd.decode('utf-8'), in_reply_to_status_id=tweet.id)
+					
+					tweetRec = "@stash_4\nRecording_Status\n"
+					if ns['channel'] == "":
+						tweetRec = tweetRec + "The program isn't being recorded."
+					else:
+						tweetRec = tweetRec + "The following program is being recorded.\nChannel: " + ns['channel'] + "\nTitle:" + ns['title'] + "\nDescription" + ns['description']
+					time.sleep(5)
+					api.PostUpdate(tweetRec.decode('utf-8'), in_reply_to_status_id=tweet.id)
+				
+				tweetNum += 1
+		
+			# API制限回避
+			time.sleep(10)
+		
+		except:
+			time.sleep(20)
